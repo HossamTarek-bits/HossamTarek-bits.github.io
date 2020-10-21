769ed8c018c8891eb26c958f3bf6d63bbdcc1365
Hossam Tarek <hossamtarek2262@gmail.com>
2020-10-21 13:51:29 +0200

    separated sections

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
 var captionLength = 0;
-var caption2 = 'An Aspiring Software Engineer'
+var caption = 'An Aspiring Software Engineer'
 var captionEl = $("#caption");
 
 
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 600);
-    type(caption2);
+    type();
+});
+
+function hoverIn (class1,class2,html){
+    $(class1).fadeOut("fast",function(e){
+        $(class2).html(html);
+    })
+}
+
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
 });
 
 
 
-
-function type(caption) {
+function type() {
     captionEl.html(caption.substr(0, captionLength++));
     if(captionLength < caption.length+1) 
-        setTimeout(function(){type(caption)}, 100);
+        setTimeout('type()', 70);
     else 
         captionLength = 0;
-}
-
-
-function erase(caption) {
-    captionEl.html(caption.substr(0, captionLength--));
-    if(captionLength >= 0)
-        setTimeout(function(){erase(caption)}, 100);
-    else
-        captionLength = 0;	
 }
 
 function cursorAnimation() {
